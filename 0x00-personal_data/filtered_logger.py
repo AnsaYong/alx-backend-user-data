1bf51fec33dfa84880942d28d15bc5f0ab6a2b6f
Ansahmbom <ansahmbomyong@gmail.com>
2024-05-17 12:39:29 +0300

    updated filtered_logger.py by adding the RedactingFormatter class and the format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 This module provides a function that returns the log message obfuscated
 """
 import re
+import logging
 from typing import List
 
 
@@ -22,3 +23,32 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class
+    """
+    REDACTION = '***'
+    FORMAT = '[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s'
+    SEPARATOR = ';'
+
+    def __init__(self, fields: List[str]):
+        """Constructor method
+
+        Args:
+            fields (List[str]): a list of fields
+        """
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format method
+
+        Args:
+            record (logging.LogRecord): a record object
+
+        Returns:
+            str: a formatted string
+        """
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message,
+                            self.SEPARATOR)

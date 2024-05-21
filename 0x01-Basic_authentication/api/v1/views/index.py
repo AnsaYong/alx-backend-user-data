8b559e9aca5383e4fe3e54772ff900f4fe1f29f4
Ansahmbom <ansahmbomyong@gmail.com>
2024-05-21 15:47:53 +0300

    updated api/v1/app.py to add a 403 error handler and api/v1/views/index.py to trigger a 403 error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized() -> str:
     Trigger a 401 error to test the error handler
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Trigger a 403 error - abort - Customer error pages
+    """
+    abort(403)

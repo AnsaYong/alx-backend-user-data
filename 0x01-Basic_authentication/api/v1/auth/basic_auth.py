6f12346c974cd1cc0e8c7cc3d5f0e36ea9ea8fe3
Ansahmbom <ansahmbomyong@gmail.com>
2024-05-22 12:44:29 +0300

    added a method that handles user authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -101,3 +101,34 @@ class BasicAuth(Auth):
             return None
 
         return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Overloads Auth and retrieves the User instance
+        for the request
+
+        Args:
+            - request: The request object
+
+        Returns:
+            - The current user
+            - None if the request is not authorized
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        if base64_header is None:
+            return None
+
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        if decoded_header is None:
+            return None
+
+        user_credentials = self.extract_user_credentials(decoded_header)
+        if user_credentials[0] is None or user_credentials[1] is None:
+            return None
+
+        return self.user_object_from_credentials(
+            user_credentials[0], user_credentials[1])

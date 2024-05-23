df1474991f27af28348470d2e6977a73d31eaffb
Ansahmbom <ansahmbomyong@gmail.com>
2024-05-23 15:17:38 +0300

    updated api/v1/auth/session_auth.py by adding a create_session() method to the SessionAuth class, which creates a new sessioID based on the user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,31 @@
 Session Authentication module
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ SessionAuth class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates and stores a new session ID for a user_id
+
+        Args:
+            user_id (str): user_id to create session for
+
+        Returns:
+            str: the session ID created
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        session_id = uuid.uuid4()
+        if session_id is None:
+            return None
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id

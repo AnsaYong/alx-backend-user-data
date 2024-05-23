bd51fed895a2f3ae139b36e189a6c26c56d1d148
Ansahmbom <ansahmbomyong@gmail.com>
2024-05-23 17:07:13 +0300

    updated api/v1/auth/session_auth.py to meet pycodestyle requirements

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -47,7 +47,7 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
-    
+
     def current_user(self, request=None):
         """
         Retrieve the curren user from the request

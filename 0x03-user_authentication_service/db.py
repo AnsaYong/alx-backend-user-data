2be418ce68df02beda4df77d110345b6754922bb
Ansahmbom <ansahmbomyong@gmail.com>
2024-05-28 18:15:46 +0300

    created the add_user methos in db.py

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+This module provides the DB class.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """
+    A class to represent a database.
+
+    Attributes:
+        _engine (Engine): The database engine.
+        _session (Session): The database session.
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Add a user to the database.
+
+        Args:
+            email (str): The user's email address.
+            hashed_password (str): The user's hashed password.
+
+        Returns:
+            User: The newly created user object.
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(new_user)
+        session.commit()
+        # session.refresh(new_user)  # Refresh the user object to get the ID
+        return new_user

bf57655b95e9721144d1d52275cca4abfa31c508
Ansahmbom <ansahmbomyong@gmail.com>
2024-05-29 21:16:36 +0300

    implemented a function to handle user login in app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 A simple Flask application to demonstrate the use of SQLAlchemy.
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
@@ -34,6 +34,30 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """
+    POST /sessions route for logging in a user.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        abort(401)
+
+    if not AUTH.get_user_by_email(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    if not session_id:
+        abort(401)
+
+    response = make_response(jsonify({"email": email, "message": "logged in"}), 200)
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
